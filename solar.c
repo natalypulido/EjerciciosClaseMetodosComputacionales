6d3988e76d299840153020c793180ce503eef2a1
Nataly Pulido <n.pulido200@uniandes.edu.co>
2017-07-27 14:57:59 -0500

    Semana 5

---
+++ solar.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include<math.h>
+#include<stdlib.h>
+/*
+INTENTO SOLO PARA DOS MASAS
+int main(void){
+  FILE*in;
+  int i;
+  int var;
+  int test;
+  char filename[100]="condiciones.csv";
+  printf("Writing to file: %s\n", filename);
+}
+
+  in = fopen(filename, "r")
+  if(!in){
+    printf("problems opening the file %s\n)"
+*/
+
+int main(void){
+  int lista[2];
+  int i;
+  float masa;
+  float pssx[100];
+  float pssy[100];
+  float pssz[100];
+  float vx[100];
+  float vy[100];
+  float vz[100];
+
+  float solpssx=0;
+  float solpssy=0;
+  float solpssz=0;
+
+  //condiciones iniciales//
+  int condiciones_iniciales=[2];
+  int condiciones_iniciales=[2];
+  float pssx[0]=30;
+  float pssy[0]=10;
+  float pssz[0]=40; 
+
+  return 0;
+}
